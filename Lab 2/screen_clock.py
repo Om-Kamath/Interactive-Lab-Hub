59c0eae4515c0b68c8b0d65e365b66c4d1ba5e00
Om Kamath <57721988+Om-Kamath@users.noreply.github.com>
2025-09-21 16:14:20 -0400

    Update link for Lab 2 README

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -136,7 +136,7 @@ def draw_clock():
     # Center the date below time
     date_x = (width - date_width) // 2
     date_y = time_y + time_height + 10
-    p
+    
     # Draw time in white
     draw.text((time_x, time_y), current_time, font=clock_font, fill=(255, 255, 255))
     
